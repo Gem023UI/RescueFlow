a4f90f8f6279e35730cf771adcaadd0705e9e1d0
Jemuel Malaga <malagajemuel@gmail.com>
2025-03-28 10:19:02 +0800

    final

--- incident/IncidentPDF.php
+++ incident/IncidentPDF.php
@@ -14,7 +14,7 @@ $pdf->SetAutoPageBreak(TRUE, 20);
 $pdf->AddPage();
 
 // Add BFP Logo
-$logoPath = $_SERVER['DOCUMENT_ROOT'] . '/RESCUEFLOW/incidents/uploads/bfplgo.png';
+$logoPath = $_SERVER['DOCUMENT_ROOT'] . '/RESCUEFLOW/incident/uploads/bfpncrlogo.png';
 $pdf->Image($logoPath, 85, 10, 40, 40, 'PNG', '', '', true);
 
 // Add Header Text
