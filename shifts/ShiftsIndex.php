5f3bf5523fa4c2e1ed864426b280b0345cc92c43
Jemuel Malaga <malagajemuel@gmail.com>
2025-04-01 18:35:57 +0800

    updated front page

--- shifts/ShiftsIndex.php
+++ shifts/ShiftsIndex.php
@@ -126,7 +126,7 @@ while ($row = mysqli_fetch_assoc($result)) {
     <nav id="sidebar">
         <ul>
         <li>
-            <span class="logo"><a href="../bfp/BFPIndex.php">BFP NCR Taguig S1</a></span>
+            <span class="logo">BFP NCR Taguig S1</span>
             <button onclick=toggleSidebar() id="toggle-btn">
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m313-480 155 156q11 11 11.5 27.5T468-268q-11 11-28 11t-28-11L228-452q-6-6-8.5-13t-2.5-15q0-8 2.5-15t8.5-13l184-184q11-11 27.5-11.5T468-692q11 11 11 28t-11 28L313-480Zm264 0 155 156q11 11 11.5 27.5T732-268q-11 11-28 11t-28-11L492-452q-6-6-8.5-13t-2.5-15q0-8 2.5-15t8.5-13l184-184q11-11 27.5-11.5T732-692q11 11 11 28t-11 28L577-480Z"/></svg>
             </button>
